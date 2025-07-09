96760de2a2b49b6fa68e22a0e1dd852e0c8394d5
hs-44 <harshvardhansutar4444@gmail.com>
2025-07-10 02:12:00 +0530

    Update build_and_push.sh

--- docker_python/scripts/build_and_push.sh
+++ docker_python/scripts/build_and_push.sh
@@ -1,67 +1,53 @@
 #!/bin/bash
-# set -e
-# This script shows how to build the Docker image and push it to ECR to be ready for use
-# by SageMaker.
+set -ex  # Exit on error and print commands
 
-# The argument to this script is the image name. This will be used as the image on the local
-#machine and combined with the account and region to form the repository name for ECR.
-#Algorithm Name will be the Reposistory Name that is passed as a command line parameter.
+# This script builds and pushes a Docker image to ECR.
+
 echo "Inside build_and_push.sh file"
+
 DOCKER_IMAGE_NAME=$1
 
-echo "value of DOCKER_IMAGE_NAME is $DOCKER_IMAGE_NAME"
-
-if [ "$DOCKER_IMAGE_NAME" == "" ]
-then
+if [ -z "$DOCKER_IMAGE_NAME" ]; then
     echo "Usage: $0 <image-name>"
     exit 1
 fi
+
+echo "Value of DOCKER_IMAGE_NAME is: $DOCKER_IMAGE_NAME"
+
+# Define the source directory from CodeBuild
 src_dir=$CODEBUILD_SRC_DIR
 
-# Get the account number associated with the current IAM credentials
+# Get the AWS account ID
 account=$(aws sts get-caller-identity --query Account --output text)
 
-if [ $? -ne 0 ]
-then
-    exit 255
-fi
+# Get AWS region from environment (fallback to us-west-2 if empty)
+region="${AWS_REGION:-us-west-2}"
+echo "Region is: $region"
 
-# Get the region defined in the current configuration (default to us-west-2 if none defined)
-region=$AWS_REGION
-echo "Region value is : $region"
-# If the repository doesn't exist in ECR, create it.
-ecr_repo_name=$DOCKER_IMAGE_NAME"-ecr-repo"
-echo "value of ecr_repo_name is $ecr_repo_name"
+# Set ECR repository name
+ecr_repo_name="${DOCKER_IMAGE_NAME}-ecr-repo"
+echo "ECR Repo Name is: $ecr_repo_name"
 
-# || means if the first command succeed the second will never be executed
-aws ecr describe-repositories --repository-names ${ecr_repo_name} --region $region || aws ecr create-repository --repository-name ${ecr_repo_name}
+# Create the ECR repo if it doesn't exist
+aws ecr describe-repositories --repository-names "$ecr_repo_name" --region "$region" || \
+aws ecr create-repository --repository-name "$ecr_repo_name" --region "$region"
 
-image_name=$DOCKER_IMAGE_NAME-$CODEBUILD_BUILD_NUMBER
+# Compose Docker image tag using build number
+image_name="${DOCKER_IMAGE_NAME}-${CODEBUILD_BUILD_NUMBER}"
 
-# Get the login command from ECR and execute docker login
-aws ecr get-login-password | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
+# Authenticate Docker with ECR
+aws ecr get-login-password --region "$region" | \
+docker login --username AWS --password-stdin "${account}.dkr.ecr.${region}.amazonaws.com"
 
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:$image_name"
-echo "fullname is $fullname"
-# Build the docker image locally with the image name and then push it to ECR with the full name.
+# Full image name
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:${image_name}"
+echo "Full image name: $fullname"
 
-docker build -t ${image_name} $CODEBUILD_SRC_DIR/docker_python/
-echo "Docker build after"
+# Build and tag Docker image
+docker build -t "$image_name" "$src_dir/docker_python/"
+docker tag "$image_name" "$fullname"
 
-echo "image_name is $image_name"
-echo "Tagging of Docker Image in Progress"
-docker tag ${image_name} ${fullname}
-echo "Tagging of Docker Image in Done"
-docker images
+# Push image to ECR
+docker push "$fullname"
 
-echo "Docker Push in Progress"
-docker push ${fullname}
-echo "Docker Push in Done"
-
-if [ $? -ne 0 ]
-then
-    echo "Docker Push Event did not Succeed with Image ${fullname}"
-    exit 1
-else
-    echo "Docker Push Event is Successful with Image ${fullname}"
-fi
+echo "Docker image pushed successfully: $fullname"
